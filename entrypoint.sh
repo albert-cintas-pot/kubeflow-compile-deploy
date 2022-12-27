930ccbbca0c0ded146c6cb58746246a60038a2e5
Albert Cintas <89633344+albert-cintas-pot@users.noreply.github.com>
2022-12-27 17:40:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Set up Kubeflow credentials
-echo "${KFP_CREDENTIALS_JSON}" > ~/.config/kfp/credentials.json
+mkdir -p /github/home/.config/kfp/
+echo "${KFP_CREDENTIALS_JSON}" > /github/home/.config/kfp/credentials.json
 
 python /app/src/main.py
