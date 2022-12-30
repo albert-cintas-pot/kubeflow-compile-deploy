d1256de856549ad663b701477c34f7bbe465e300
Albert Cintas <89633344+albert-cintas-pot@users.noreply.github.com>
2022-12-30 17:26:28 +0100

    New action

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import kfp
 import kfp.compiler as compiler
 import string
-import random
 import logging
 import sys
 import os
@@ -16,22 +15,6 @@ client_id = os.getenv("CLIENT_ID")
 other_client_id = os.getenv("OTHER_CLIENT_ID")
 other_client_secret = os.getenv("OTHER_CLIENT_SECRET")
 pipeline_path = os.getenv("PIPELINE_FILE_PATH")
-pipeline_func_name = os.getenv("PIPELINE_FUNC_NAME")
-pipeline_id = os.getenv("PIPELINE_ID")
-pipeline_name = os.getenv("PIPELINE_NAME")
-experiment_name = os.getenv("EXPERIMENT_NAME")
-namespace = os.getenv("NAMESPACE")
-run_pipeline = os.getenv("RUN_PIPELINE")
-
-# Set pipeline name to function name if no name is provided
-if not pipeline_name:
-    pipeline_name = pipeline_func_name
-
-# Random value used for naming
-def random_suffix() -> string:
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
-    
-run_name = 'run-' + str(pipeline_name) + "-" + random_suffix()
 
 # Authenticate session client
 client = kfp.Client(host=host, 
@@ -42,6 +25,9 @@ client = kfp.Client(host=host,
 )
 
 # Compile pipeline
+pipeline_name = pipeline_path.split('/')[-1].rsplit('.', 1)[0]
+pipeline_func_name = pipeline_name
+
 def load_pipeline_from_path(
     pipeline_func_name: str, pipeline_path: str
 ) -> staticmethod:
@@ -60,32 +46,41 @@ pipeline_function = load_pipeline_from_path(
 zip_name = pipeline_function.__name__ + ".zip"
 compiler.Compiler().compile(pipeline_function, zip_name)
 
-# Get new version name
-version_obj = client.pipelines.list_pipeline_versions(
-    resource_key_id=str(pipeline_id)
-)
-version_count = version_obj.total_size
-version = int(version_count) + 1
+# Check if pipeline exists
+pipeline_name = pipeline_path.split('/')[-1].rsplit('.', 1)[0]
+pipeline_func_name = pipeline_name
 
-# Upload pipeline into new version
-client.pipeline_uploads.upload_pipeline_version(
-    zip_name,
-    name=version,
-    pipelineid=pipeline_id
-)
+# Add a filter for the list_pipelines method
+filter = json.dumps({'predicates': [{'key': 'name', 'op': 1, 'string_value': '{}'.format(pipeline_name)}]})
+filter_pipeline = client.pipelines.list_pipelines(filter=filter)
 
-# Get experiment_id from experiment_name
-experiment_id = client.get_experiment(
-    experiment_name=experiment_name
-).id
+try:
+    does_exist = filter_pipeline.pipelines[0]
+    pipeline_exists = True
+except TypeError:
+    pipeline_exists = False
 
-# Run uploaded pipeline
-if run_pipeline == "True":
-    client.run_pipeline(
-        pipeline_id=pipeline_id,
-        experiment_id=experiment_id,
-        job_name=run_name
-        )
-    logging.info(f"A run is created with: {run_name}")
-else:
+if pipeline_exists:
+    # Get new version name
+    version_obj = client.pipelines.list_pipeline_versions(
+        resource_key_id=str(pipeline_id)
+    )
+    version_count = version_obj.total_size
+    version = int(version_count) + 1
+    
+    # Upload pipeline into new version
+    client.pipeline_uploads.upload_pipeline_version(
+        zip_name,
+        name=version,
+        pipelineid=pipeline_id
+    )
+
     logging.info(f"Version {version} has been created for pipeline: {pipeline_name}")
+
+# If the pipeline does not exist, upload new one
+else:
+    client.pipeline_uploads.upload_pipeline_version(
+        zip_name,
+        pipeline_name=pipeline_name,
+    )
+    logging.info(f"New pipeline '{pipeline_name}'  has been deployed")
